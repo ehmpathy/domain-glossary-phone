47f530b76b2c56066895b8162cbcfb608dddf722
Ulad Kasach <uladkasach@gmail.com>
2024-12-03 02:00:45 -0800

    fix(check): extend isUniPhoneNumber withAssure

--- src/isUniPhoneNumber.ts
+++ src/isUniPhoneNumber.ts
@@ -1,15 +1,19 @@
+import { withAssure } from 'type-fns';
+
 import { UniPhoneNumber } from './UniPhoneNumber';
 import { asUniPhoneNumber } from './asUniPhoneNumber';
 
 /**
  * checks whether a string is a universally formatted phone number
  */
-export const isUniPhoneNumber = (input: string): input is UniPhoneNumber => {
-  try {
-    const universal = asUniPhoneNumber(input);
-    return input === universal;
-  } catch {
-    // if there was an error, then its not a phone number, so defo not a universal phone number
-    return false;
-  }
-};
+export const isUniPhoneNumber = withAssure(
+  (input: string): input is UniPhoneNumber => {
+    try {
+      const universal = asUniPhoneNumber(input);
+      return input === universal;
+    } catch {
+      // if there was an error, then its not a phone number, so defo not a universal phone number
+      return false;
+    }
+  },
+);
